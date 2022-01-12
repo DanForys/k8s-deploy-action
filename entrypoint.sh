8d142b7e876c82636ee3d014a0f6edbc97e4c3c2
Dan Forys <d@forys.co.uk>
2022-01-12 12:34:41 +0000

    WIP

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ echo ${KUBE_CONFIG_DATA} | base64 -d > kubeconfig
 export KUBECONFIG=kubeconfig
 
 echo ::echo::on
-kubectl set image deployment/${INPUT_DEPLOYMENT} ${INPUT_CONTAINER_NAME}=${INPUT_IMAGE}
+kubectl set image deployment/${INPUT_DEPLOYMENT} ${INPUT_CONTAINER-NAME}=${INPUT_IMAGE}
 deployStatus=$?
 if [[ $deployStatus -ne 0 ]]; then exit 1; fi
 
