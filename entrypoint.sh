09201a74366e43931325c5b4d90082030ec130c9
Dan Forys <d@forys.co.uk>
2024-09-13 20:21:33 +0100

    WIP

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ echo -e "${INPUT_TEMPLATE_VALUES}" > template_values.yaml
 
 cat ./template_values.yaml
 
-if [[ INPUT_DELETE ]]; then
+if [[ -n INPUT_DELETE ]]; then
   ytt -f ${INPUT_TEMPLATE_DIR} --data-values-file ./template_values.yaml | kubectl delete -f -
   exit $?
 fi
