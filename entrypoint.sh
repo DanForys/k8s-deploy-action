81db4ebaaf8a432fe66eadee13c85a9c9076994f
Dan Forys <d@forys.co.uk>
2024-09-13 11:15:31 +0100

    Update to use ytt

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,14 @@ echo ${KUBE_CONFIG_DATA} | base64 -d > kubeconfig
 export KUBECONFIG=kubeconfig
 
 echo ::echo::on
-kubectl set image deployment/${INPUT_DEPLOYMENT} ${INPUT_CONTAINER_NAME}=${INPUT_IMAGE}
-deployStatus=$?
-if [[ $deployStatus -ne 0 ]]; then exit 1; fi
+echo ${INPUT_TEMPLATE_VALUES} > template_values.yaml
 
-kubectl rollout status deployment/${INPUT_DEPLOYMENT}
-rolloutStatus=$?
-exit $rolloutStatus
+ytt -f ${INPUT_TEMPLATE_DIR} --data-values-file ./template_values.yaml
+
+# kubectl set image deployment/${INPUT_DEPLOYMENT} ${INPUT_CONTAINER_NAME}=${INPUT_IMAGE}
+# deployStatus=$?
+# if [[ $deployStatus -ne 0 ]]; then exit 1; fi
+
+# kubectl rollout status deployment/${INPUT_DEPLOYMENT}
+# rolloutStatus=$?
+# exit $rolloutStatus
